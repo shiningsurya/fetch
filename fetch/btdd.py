74613c71391dbd7b626564beae85868ab9e20bf2
shiningsurya <15906282+shiningsurya@users.noreply.github.com>
2021-04-06 15:10:22 +0200

    do not use btdd; use btdd_m

--- fetch/btdd.py
+++ fetch/btdd.py
@@ -4,7 +4,8 @@
 Should I cython?
 
 FETCH seems to this:
-    - For a candidate at DM, FETCH.bowtie plane goes from -DM to DM with ndm steps
+    - For a candidate at DM, FETCH.bowtie plane goes from 0 to 2*DM with ndm steps
+    - the time resolution in the bowtie and the de-dispersed filterbank is half-the-width
 """
 
 import numpy as np
@@ -14,20 +15,49 @@ import fetch.incoherent as fetch_incoherent
 
 DM_FAC = 4.148E-3
 
+BT_INCOH = False
+"""
+FDMT is finnicky. 
+If the max-delay is less than number-of-channels, FDMT is no longer the best. 
+In that case, we would have to take a staggered approach
+
+BT_INCOH is the switch which does not use FDMT to compute bowtie. Instead it does repeated de-dispersion.
+It is slow. It is not exposed to the user and the supporting code will be removed soon.
+"""
+
 class BTDD:
     """
     Class to streamline computation of bowtie plane and de-dispersed filterbank.
+
+    FDMT algorithm requires the number of DM-trials to be less than number-of-chans, 
+    but since 
+        (1) FETCH DM range is [0., 2 *DM] where DM is the dm of the burst,
+        (2) and when dealing with data with high time resolution and low frequency resolution
+
+    The FDMT implementation here is that of a staggarred approach.
+    Bowtie (0..D) -------------------------> Bowtie (0..A)
+        |
+        \-----> Incoherent (A) ------------> Bowtie (0..A)
+                    |
+                    \---> Incoherent (A) --> Bowtie (0..A)
+
+    where D is some large DM-delay-full-band in time units
+    where A is the number of channels
+
+    In some future, we could optimize like anything since 
+    for a given obsparam (nchans,fch1,foff), bowtie(0..A) can be resolved in one pass
 
     This class doesnot make use of the optimization trick.
     Bowtie (A..B)  --> Incoherent (A) & Bowtie (B-A)
     Incoherent (C) --> Incoherent (A) & Incoherent (C-A)
     where A,B,C are delays
 
+
     Because FETCH DM range always start at DM=0
     """
     def __init__ (self,
             fch1=None,foff=None,nchans=None,
-            tsamp=None, nsamps=None,
+            tsamp=None, 
             ndm=None,
         ):
         """
@@ -53,6 +83,11 @@ class BTDD:
         self.fdmt    = fetch_fdmt.FDMT (self.fch1, self.foff, self.nchans)
         self.bt_delays = None
         self.dd_delays = None
+        self.max_d     = None
+        if BT_INCOH:
+            self.bb_delays = np.zeros ((self.ndm, self.nchans), dtype=np.uint64)
+        else:
+            self.bb_delays = None
 
 
     def __call__ (self, dm):
@@ -62,16 +97,30 @@ class BTDD:
         ## dm calculations
         dm_axis        = dm + np.linspace (-dm, dm, self.ndm)
         self.bt_delays      = np.zeros (self.ndm, dtype=np.uint32)
-        for i,idm in enumerate (dm_axis):
-            self.bt_delays[i] = int (idm * self.fullband_delay / self.tsamp)
+
+        if BT_INCOH:
+            for i,idm in enumerate (dm_axis):
+                self.bt_delays[i] = int (idm * self.fullband_delay / self.tsamp)
+                self.bb_delays[i] = np.uint64 (self.inband_delay * idm / self.tsamp)
+        else:
+            for i,idm in enumerate (dm_axis):
+                self.bt_delays[i] = int (idm * self.fullband_delay / self.tsamp)
+
         self.dd_delays      = np.uint64 (self.inband_delay * dm / self.tsamp)
+        self.max_d          = self.bt_delays[-1]
 
     def work (self, fb):
         """
         fb is (nchans, nsamps)
         """
-        ## actual algo
-        bt             = self.fdmt.Bowtie_delays (fb, self.bt_delays)
+        nchans, nsamps = fb.shape
+        if BT_INCOH:
+            osamps         = int(nsamps - self.bb_delays.max())
+            bt             = np.zeros ((self.ndm, osamps), dtype=fb.dtype)
+            for i in range (self.ndm):
+                bt[i,:osamps]      = fetch_incoherent.Dedisperser (fb, self.bb_delays[i]).mean (axis=0)[:osamps]
+        else:
+            bt             = self.fdmt.Bowtie_delays (fb, self.bt_delays)
         dd             = fetch_incoherent.Dedisperser (fb, self.dd_delays)
 
         ## reset delay arrays
